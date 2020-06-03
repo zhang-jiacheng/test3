9094ae662aa7864cbd0b193fb478d3855f093ca6
zhangjiacheng <2500812818@qq.com>
2020-06-03 15:45:38 +0900

    test

---
+++ test01/src/Test01.java
@@ -0,0 +1,19 @@
+/**
+ * @Package PACKAGE_NAME
+ * @author zjc
+ * @date 2020/6/3 15:37
+ * @version V1.0
+ * @Copyright © 株式会社多言语系统研究所
+ */
+
+/**
+ * @program: ideaTestProjects
+ * @description: test01
+ * @author: lc
+ ** @create: 2020-06-03 15:37
+ **/
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
